49949f191230922bd6cd6f42a7eff0dec34c0176
SamP-S <sam.ps1@outlook.com>
2024-02-29 04:02:37 +0000

    Add saving eval data per epoch; Clean up; hyper params as cfg struct

--- pytorch_resnet.py
+++ pytorch_resnet.py
@@ -10,29 +10,40 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+from tqdm import tqdm
+import pandas as pd
 
-### --- DISPLAY FIRST IMAGE BATCH
-# take grid of images and title as string and display using matplotlib
-def imshow(inp, title):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    plt.title(title)
-    plt.show()
+from setup_env import setup_output_dir
+
+
+class HyperParams:
+    NUM_EPOCHS = 8
+    LOAD_FROM = None
+    MODEL_NAME = "resnet"
+    MODELS_DIR = "./models"
 
 ### --- TRAIN MODEL
 # Abstract model training function
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(model, criterion, optimizer, scheduler, hp):
     since = time.time()
 
-    best_model_wts = copy.deepcopy(model.state_dict())
+    best_model = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1)) 
-        print('-' * 10)
+    for epoch in range(hp.NUM_EPOCHS):
+        print(f"Epoch {epoch}/{hp.NUM_EPOCHS - 1}") 
+        print("-----------------")
+
+
+        losses = {
+            "train": np.zeros(shape=(len(dataloaders["train"])), dtype=np.float32),
+            "val": np.zeros(shape=(len(dataloaders["val"])), dtype=np.float32),
+        }
+
+        accuracies = {
+            "train": np.zeros(shape=(len(dataloaders["train"])), dtype=np.float32),
+            "val": np.zeros(shape=(len(dataloaders["val"])), dtype=np.float32),
+        }
 
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
@@ -41,67 +52,72 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             else:
                 model.eval()   # Set model to evaluate mode
 
-            running_loss = 0.0
-            running_corrects = 0
+            
 
             # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
+            for idx, (inputs, labels) in enumerate(tqdm(dataloaders[phase])):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
                 # forward
                 # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
+                with torch.set_grad_enabled(phase == "train"):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
                     # backward + optimize only if in training phase
-                    if phase == 'train':
+                    if phase == "train":
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                losses[phase][idx] = loss.item() * inputs.size(0)
+                accuracies[phase][idx] = torch.sum(preds == labels.data) / len(labels)
 
-            if phase == 'train':
+            if phase == "train":
                 scheduler.step()
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            phase_loss = np.sum(losses[phase]) / dataset_sizes[phase]
+            phase_acc = np.sum(accuracies[phase]) / dataset_sizes[phase]
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            df_results = pd.DataFrame({
+                "train_loss": losses["train"],
+                "train_acc": accuracies["train"],
+                "eval_loss": losses["val"],
+                "eval_acc": accuracies["val"]
+            })
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
+            print(f"{phase}: \tloss={phase_loss:.4f}, \tacc={phase_acc:.4f}")
+            torch.save(model, os.path.join(hp.MODEL_DIR, hp.MODEL_NAME, f"_{epoch}.bin"))
+            df_results.to_csv(os.path.join(hp.MODEL_DIR, hp.MODEL_NAME, f"_{epoch}_eval.csv"))
 
-        print()
+            if phase == "val":
+                if phase_acc > best_acc:
+                    best_acc = phase_acc
+                    best_model = copy.deepcopy(model.state_dict())
 
     time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print(f'Training complete in {time_elapsed // 60}m {time_elapsed % 60}s')
 
     # load best model weights
-    model.load_state_dict(best_model_wts)
+    model.load_state_dict(model)
     return model
 
 
 if __name__ == "__main__":
+    hy_params = HyperParams()
+    hy_params.LOAD_FROM = None
+    hy_params.NUM_EPOCHS = 24
+    hy_params.DATA_DIR = "./data/bugs"
+    hy_params.MODEL_DIR = setup_output_dir("./models/bugs")
+    hy_params.MODEL_NAME = "resnet"
+
     ### --- PARALLELISE TRAINING
-    # set device as GPU if available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print ("DEVICE: ", device)
 
-
     ### --- DATASET CONFIGURATION AND SETUP
-    DATA_DIR = "./data/bricks/26_bricks_augmented"
-
     # fixed arrays for normalisation
     MEAN = np.array([0.5, 0.5, 0.5])
     STD = np.array([0.25, 0.25, 0.25])
@@ -109,22 +125,22 @@ if __name__ == "__main__":
     data_transforms = {
         # transformation of images for training
         'train': transforms.Compose([
-            transforms.RandomResizedCrop(224),  # random crop to grab different part of image
-            transforms.RandomHorizontalFlip(),  # random flipping to vary image
+            transforms.Resize(224),
+            transforms.CenterCrop(224), 
             transforms.ToTensor(),
-            transforms.Normalize(MEAN, STD)     # normalises image for colour channels to be arround the centre
+            transforms.Normalize(MEAN, STD)
         ]),
         # transformation of images for validation
         'val': transforms.Compose([
-            transforms.Resize(256),         # resize to standard
-            transforms.CenterCrop(224),     # crop to object
+            transforms.Resize(224),
+            transforms.CenterCrop(224), 
             transforms.ToTensor(),          
             transforms.Normalize(MEAN, STD)
         ]),
     }
 
     # create dataset from folder structure
-    image_loader = datasets.ImageFolder(DATA_DIR)
+    image_loader = datasets.ImageFolder(hy_params.DATA_DIR)
 
     # split into train and validation datasets according to ratio
     TRAIN_VAL_RATIO = 0.2
@@ -149,77 +165,42 @@ if __name__ == "__main__":
     class_names = image_loader.classes
     print(class_names)
 
-    # Get a batch (4) of training data
-    inputs, classes = next(iter(dataloaders['train']))
-    # Make image grid from batch
-    out = torchvision.utils.make_grid(inputs)
-    # display
-    # imshow(out, title=[class_names[x] for x in classes])
-
-
 
     ### --- MODEL CONFIGURATION
-    # https://pytorch.org/vision/stable/models.html
-    # Load a pretrained model and reset final fully connected layer.
     # model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT) 
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)   
     # model = models.alexnet(weights=models.AlexNet_Weights.DEFAULT)
     # model = models.GoogLeNet()
 
     #### ConvNet as fixed feature extractor ####
-    # Here, we need to freeze all the network except the final layer.
-    # We need to set requires_grad == False to freeze the parameters so that the gradients are not computed in backward()
-    IS_FIXED_FEATURE_EXTRACTOR = True
+    IS_FIXED_FEATURE_EXTRACTOR = False
     if IS_FIXED_FEATURE_EXTRACTOR:
-        for param in model.parameters():   # Iterate through all layers of network
+        for param in model.parameters():        # Iterate through all layers of network
             param.requires_grad = False         # Freeze layer so it is not trainable
-    # Replacing final Fully Connected (FC) output layer
-    # As a new layer is created, requires_grad == True so it is retrained
-    num_ftrs = model.fc.in_features     # get number of input features at FC layer
+                                                # Replacing final Fully Connected (FC) output layer
+                                                # As a new layer is created, requires_grad == True so it is retrained
+    num_ftrs = model.fc.in_features             # get number of input features at FC layer
     model.fc = nn.Linear(num_ftrs, len(class_names)) # assign new layer with output resized to our number of classes
-    model = model.to(device)    # send to device
+    model = model.to(device)                    # send to device
 
 
 
     ### --- CRITERION CONFIGURATION
-    # https://pytorch.org/docs/stable/nn.html#loss-functions
-    # Criterion determines the loss function of model to determine current performance
-    #
-    # nn.L1Loss     - measures the mean absolute error (MAE) between each element in the input xx and target yy
-    # nn.MSELoss    - measures the mean squared error (squared L2 norm) between each element in the input xx and target yy.
-    # nn.CrossEntropyLoss   - computes the cross entropy loss between input logits and target.
     criterion = nn.CrossEntropyLoss()
 
 
 
     ### --- OPTIMIZER CONFIGURATION
-    # https://pytorch.org/docs/stable/optim.html
-    # Optimizer algorithm determines how the loss function is used to update the model's parameter
-    #
-    # Observe that all parameters are being optimized
-    # using stochastic gradient descent (optionally with momentum).
     optimizer = optim.SGD(model.parameters(), lr=0.001) 
     if IS_FIXED_FEATURE_EXTRACTOR:
         optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
 
 
     ### --- SCHEDULER CONFIGURATION
-    # https://pytorch.org/docs/stable/optim.html
-    # LR Scheduler can adjust the learning rate based on the number of epochs completed
-    #
-    # StepLR Decays the learning rate of each parameter group by gamma every step_size epochs
-    # Decay LR by a factor of 0.1 every 7 epochs
-    # Learning rate scheduling should be applied after optimizer’s update
-    # e.g., you should write your code this way:
-    # for epoch in range(100):
-    #     train(...)
-    #     validate(...)
-    #     scheduler.step()
-    # Decay LR by a factor of 0.1 every 7 epochs
     step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 
 
     ### --- TRAIN MODEL
-    model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=25)
+    model = train_model(model, criterion, optimizer, step_lr_scheduler, hp=hy_params)
 
