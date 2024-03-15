7c75f69d901c860e7ac812e5807f9dc170523ec7
SamP-S <sam.ps1@outlook.com>
2024-03-15 16:40:16 +0000

    unify model transfer impl; clean up

--- pytorch_resnet.py
+++ pytorch_resnet.py
@@ -12,18 +12,19 @@ import os
 import copy
 from tqdm import tqdm
 import pandas as pd
+import sys
 
 from setup_env import setup_output_dir
 
 
 class HyperParams:
     NUM_EPOCHS = 8
+    BATCH_SIZE = 32
     LOAD_FROM = None
     MODEL_NAME = "resnet"
     MODELS_DIR = "./models"
 
 ### --- TRAIN MODEL
-# Abstract model training function
 def train_model(model, criterion, optimizer, scheduler, hp):
     since = time.time()
 
@@ -48,25 +49,20 @@ def train_model(model, criterion, optimizer, scheduler, hp):
         # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                model.train()  # Set model to training mode
+                model.train()
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()
 
-            
-
-            # Iterate over data.
             for idx, (inputs, labels) in enumerate(tqdm(dataloaders[phase])):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
                 # forward
-                # track history if only in train
                 with torch.set_grad_enabled(phase == "train"):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
-                    # backward + optimize only if in training phase
                     if phase == "train":
                         optimizer.zero_grad()
                         loss.backward()
@@ -114,28 +110,26 @@ if __name__ == "__main__":
     hy_params = HyperParams()
     hy_params.LOAD_FROM = None
     hy_params.NUM_EPOCHS = 24
-    hy_params.DATA_DIR = "./data/26_bricks_augmented_half"
-    hy_params.MODEL_DIR = setup_output_dir("./models/26_bricks_augmented_half")
-    hy_params.MODEL_NAME = "resnet"
+    hy_params.BATCH_SIZE = 32
+    hy_params.DATA_DIR = "data/augmented/old_26"
+    hy_params.MODEL_DIR = setup_output_dir("data/models/old_26")
+    hy_params.MODEL_NAME = "alexnet"
 
     ### --- PARALLELISE TRAINING
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print ("DEVICE: ", device)
 
     ### --- DATASET CONFIGURATION AND SETUP
-    # fixed arrays for normalisation
     MEAN = np.array([0.5, 0.5, 0.5])
     STD = np.array([0.25, 0.25, 0.25])
 
     data_transforms = {
-        # transformation of images for training
         'train': transforms.Compose([
             transforms.Resize(224),
             transforms.CenterCrop(224), 
             transforms.ToTensor(),
             transforms.Normalize(MEAN, STD)
         ]),
-        # transformation of images for validation
         'val': transforms.Compose([
             transforms.Resize(224),
             transforms.CenterCrop(224), 
@@ -144,68 +138,63 @@ if __name__ == "__main__":
         ]),
     }
 
-    # create dataset from folder structure
     image_loader = datasets.ImageFolder(hy_params.DATA_DIR)
 
-    # split into train and validation datasets according to ratio
     TRAIN_VAL_RATIO = 0.2
     val_size = int(TRAIN_VAL_RATIO * len(image_loader))
     train_size = len(image_loader) - val_size
     train_dataset, val_dataset = random_split(image_loader, [train_size, val_size])
 
-    # assemble into dataset dictionary and set transforms
     image_datasets = {'train': train_dataset, 'val': val_dataset}
     for key, val in image_datasets.items():
         val.dataset.transform = data_transforms[key]
     
-    # create dictionary of dataloaders using dataset dictionary
-    #   BATCH SIZE = 4;
-    #   SHUFFLE = TRUE;
-    #   NUM_WORKERS = 0;
-    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=0)
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=hy_params.BATCH_SIZE, shuffle=True, num_workers=0)
                         for x in ['train', 'val']}
-    # create dictionary of dataset sizes for training and valuation
+
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    # get classes from image loader
     class_names = image_loader.classes
     print(class_names)
 
-
     ### --- MODEL CONFIGURATION
-    # model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT) 
-    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)   
-    # model = models.alexnet(weights=models.AlexNet_Weights.DEFAULT)
-    # model = models.GoogLeNet()
+    model = None
+    if (hy_params.MODEL_NAME == "resnet18"):
+        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+    elif (hy_params.MODEL_NAME == "resnet34"):
+        model = models.resnet34(weights=models.ResNet50_Weights.DEFAULT)
+    elif (hy_params.MODEL_NAME == "resnet50"):
+        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+    elif (hy_params.MODEL_NAME == "resnet101"):
+        model = models.resnet101(weights=models.ResNet50_Weights.DEFAULT)
+    elif (hy_params.MODEL_NAME == "resnet152"):
+        model = models.resnet152(weights=models.ResNet50_Weights.DEFAULT)
+    elif (hy_params.MODEL_NAME == "mobilenet_v3_large"):
+        model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
+        model.fc = model.classifier[-1]
+    elif (hy_params.MODEL_NAME == "mobilenet_v3_small"):
+        model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
+        model.fc = model.classifier[-1]
+    elif (hy_params.MODEL_NAME == "alexnet"):
+        model = models.alexnet(weights=models.AlexNet_Weights.DEFAULT)
+        model.fc = model.classifier[-1]
+    else:
+        print(f"ERROR: Invalid model name @ {hy_params.MODEL_NAME}")
+        sys.exit()
 
-    #### ConvNet as fixed feature extractor ####
     IS_FIXED_FEATURE_EXTRACTOR = False
     if IS_FIXED_FEATURE_EXTRACTOR:
-        for param in model.parameters():        # Iterate through all layers of network
-            param.requires_grad = False         # Freeze layer so it is not trainable
-                                                # Replacing final Fully Connected (FC) output layer
-                                                # As a new layer is created, requires_grad == True so it is retrained
-    num_ftrs = model.fc.in_features             # get number of input features at FC layer
-    model.fc = nn.Linear(num_ftrs, len(class_names)) # assign new layer with output resized to our number of classes
-    model = model.to(device)                    # send to device
+        for param in model.parameters():
+            param.requires_grad = False
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, len(class_names))
+    model = model.to(device)                    
 
-
-
-    ### --- CRITERION CONFIGURATION
     criterion = nn.CrossEntropyLoss()
-
-
-
-    ### --- OPTIMIZER CONFIGURATION
-    optimizer = optim.SGD(model.parameters(), lr=0.001) 
+    optimizer = optim.SGD(model.parameters(), lr=0.01) 
     if IS_FIXED_FEATURE_EXTRACTOR:
-        optimizer = optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
+        optimizer = optim.SGD(model.fc.parameters(), lr=0.01, momentum=0.9)
 
-
-    ### --- SCHEDULER CONFIGURATION
     step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-
-
-    ### --- TRAIN MODEL
     model = train_model(model, criterion, optimizer, step_lr_scheduler, hp=hy_params)
 
