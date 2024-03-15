68d0cab2f4c6635df74fb8ff13909d3f6ff1c6f7
SamP-S <sam.ps1@outlook.com>
2024-03-15 15:53:58 +0000

    move doc comments to README

--- pytorch_cuda_check.py
+++ pytorch_cuda_check.py
@@ -2,7 +2,7 @@ import torch
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-print("Assuming that we are on a CUDA machine, this should print a CUDA device:")
+print("Check if CUDA is available:")
 print(torch.cuda.is_available())
 print(torch.version)
 print(device)
